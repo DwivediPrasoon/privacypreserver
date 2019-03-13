4c4369c3011db1690e4350ad251926aa23b715c3
IIITB <iiitb@Dhaneshkumars-Mac-mini.local>
2019-03-13 14:46:18 +0530

    Heirarchies added

--- src/org/gui/pp/ARXMain.java
+++ src/org/gui/pp/ARXMain.java
@@ -1,9 +1,14 @@
 package org.gui.pp;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.FocusAdapter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -17,9 +22,13 @@ public class ARXMain extends JFrame{
     private JTable table1;
     private JTable table2;
     private JButton nextButton;
+    private JList list1;
+    private JTable table3;
+    private JButton addColumnButton;
     JFileChooser fc;
     String[] attributeList;
     HashMap<String,String> attributeSensitivity;
+    HashMap<String,HashSet<String>> attributeDomain;
 
 
     public ARXMain() {
@@ -38,8 +47,8 @@ public class ARXMain extends JFrame{
                     CSVFile Rd = new CSVFile();
                     ArrayList<String[]> Rs2 = Rd.ReadCSVfile(file);
                     attributeList = Rs2.get(0);
-                    System.out.println(attributeList.length);
                     Rs2.remove(0);
+                    createDomainForAttributes(Rs2);
                     MyModel NewModel = new MyModel(attributeList);
                     table1.setModel(NewModel);
                     NewModel.AddCSVData(Rs2);
@@ -62,9 +71,11 @@ public class ARXMain extends JFrame{
                 for(int i=0; i< table2.getRowCount(); i++){
                     attributeSensitivity.put(attributeList[i], (String) mod.getValueAt(i,1));
                 }
-
+                tabbedPane1.setSelectedIndex(2);
+                heirarchies();
             }
         });
+
     }
 
 
@@ -87,10 +98,39 @@ public class ARXMain extends JFrame{
         renderer.setToolTipText("Click to Choose Sensitivity");
         tc.setCellRenderer(renderer);
 
+
     }
 
+    public void heirarchies(){
+        ListModel<String> lm = new AbstractListModel<String>() {
+            @Override
+            public int getSize() {
+                return attributeList.length;
+            }
 
+            @Override
+            public String getElementAt(int index) {
+                return attributeList[index];
+            }
+        };
 
+        list1.setModel(lm);
+    }
+
+    public void createDomainForAttributes(ArrayList<String[]> dataRows){
+        attributeDomain = new HashMap<>();
+        for(int columnNumber = 0; columnNumber < attributeList.length; columnNumber++ ){
+            for(int rowNumber = 0; rowNumber < dataRows.size(); rowNumber++){
+                if(attributeDomain.containsKey(attributeList[columnNumber])){
+                    attributeDomain.get(attributeList[columnNumber]).add(dataRows.get(rowNumber)[columnNumber]);
+                }
+                else{
+                    attributeDomain.put(attributeList[columnNumber], new HashSet<>());
+                }
+            }
+        }
+        //System.out.println(attributeDomain.toString());
+    }
 
 
 }
